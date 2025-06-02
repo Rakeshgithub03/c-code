c1dcec9c40f01f4834bfa7c61a0735db41fd5f2c
Rakeshgithub03 <rakeshpal03042002@gmail.com>
2025-06-02 22:31:44 +0530

    Add files via upload

---
+++ 01_printelementofarray.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+int n,i;
+printf("enter size of array: ");
+scanf("%d",&n);
+int A[n];
+printf("enter %d number: ",n);
+for(i=0;i<n;i++){
+    scanf("%d",&A[i]);
+}
+printf("the array is ");
+for(i=0;i<n;i++){
+    
+printf(" %d",A[i]);
+    
+}
+return 000000000;
+}
